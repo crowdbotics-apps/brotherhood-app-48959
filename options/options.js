b9d10a387d5cbe9575a1a382a60f766765cd90d8
Crowdbotics <team@crowdbotics.com>
2024-08-05 19:22:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "brotherhood-app-48959",
+  url: "https://brotherhood-app-48959.botics.co",
+  api: "https://brotherhood-app-48959.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
